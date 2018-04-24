572d23a0d95a7e6701f673cdd60d9e4cee05e4f0
Nick <nweld568@gmail.com>
2018-04-24 08:54:38 -0400

    Added connection to Cloud database

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
 
-$hn = 'localhost';
-$db = '411';
-$un = 'root';
+$hn = '35.194.83.205';
+$db = 'partier';
+$un = 'user';
 $pw = '';
 
-$conn = new mysqli($hn, $un, $pw, $db);
+$conn = new mysqli($hn, $un, $pw, $db,null,'/cloudsql/partier-202001:us-east4:event-planner');
 
 //echo "connection successful";
 
