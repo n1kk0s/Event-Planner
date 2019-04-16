16d5d8e0febfbc7391081236f53343ba103d735e
Nick <nweld568@gmail.com>
2019-04-16 13:12:42 -0400

    Updated connect.php
    Different database connection credentials

--- connect.php
+++ connect.php
@@ -2,8 +2,8 @@
 
 $hn = '35.231.116.124';
 $db = 'partier';
-$un = 'user';
-$pw = '';
+$un = 'root';
+$pw = 'toor';
 
 $conn = new mysqli($hn, $un, $pw, $db,null,'partier-202001:us-east1:partier');
 
